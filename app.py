ee934a486f6691cae62282596546d9fd24df0722
oluwatobi1 <akintunlesetobi@gmail.com>
2022-06-17 13:15:44 +0100

    flask todo

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]= 'postgresql://tobi:1234@localhost:5432/todoapp'
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todo'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<Todo {self.description}>'
+
+db.create_all()
+
+
+
+@app.route('/')
+def index(): 
+    return render_template("index.html", data = Todo.query.all())
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
